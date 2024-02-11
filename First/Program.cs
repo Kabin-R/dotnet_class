8ff33ebeb050729f26dfb38524db79058dbf26b1
Kabin Rimal <kabinrimal@gmail.com>
2024-02-11 18:08:20 +0545

    program to find sum of two numbers.

--- First/Program.cs
+++ First/Program.cs
@@ -10,6 +10,15 @@ namespace First
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter forst number");
+            String num1=Console.ReadLine();
+
+            Console.WriteLine("Enter second number");
+            String num2 = Console.ReadLine();
+
+            Console.WriteLine(Convert.ToInt32(num1)+Convert.ToInt32(num2));
+            Console.ReadKey();
+
         }
     }
 }
